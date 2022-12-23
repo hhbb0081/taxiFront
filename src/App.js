341543787a415290f5cf5f387f013f43bd3d21ff
namkijoo <rlwn5904@naver.com>
2022-12-24 00:33:04 +0900

    second

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reactsadfasdfsdaf
+          Learn Reactsadfasdfsdafafadsfa
         </a>
       </header>
     </div>
