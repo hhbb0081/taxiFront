7bbbc47295864a634d11bb13f5bc2e975148a9bd
hhbb0081 <likesun2000@naver.com>
2023-01-24 19:28:09 +0900

    chore: 띄어쓰기

--- src/components/headerForm.jsx
+++ src/components/headerForm.jsx
@@ -3,12 +3,15 @@ import TAXI from './taxi.jpg';
 import styles from './header.module.css';
 
 export default function HeaderForm() {
+  
   return (
     <div className={styles.header}>
       <div className={styles.logo}>
         <img src={TAXI} alt="택시 사진" />
       </div>
-      <div className={styles.title}>TAXI</div>
+      <div
+        className={styles.title}
+      >TAXI</div>
     </div>
   )
 }
